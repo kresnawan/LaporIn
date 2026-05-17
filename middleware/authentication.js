f04b39b5de8fb195354c456737b8ce60a801fb21
kresnawan <sanjayakresnawan@gmail.com>
2026-05-17 11:46:15 +0700

    Add auth middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,29 +2,25 @@ import jwt from 'jsonwebtoken';
 import 'dotenv/config'
 const accessTokenKey = process.env.ACCESS_TOKEN_KEY;
 
-export const authenticateToken = (req, res, next) => {
-    try {
-        const accessToken = req.headers.authorization.split(" ")[1];
-
-        if (!accessToken) {
-            // return res.status(401).send("Access token tidak ditemukan");
-            next();
+export const authenticateToken = (mustLoggedIn) => {
+    return (req, res, next) => {
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken && !mustLoggedIn) {
+            return next();
         }
+        
+        try {
+            const accessToken = req.headers.authorization.split(" ")[1];
 
-        jwt.verify(accessToken, accessTokenKey, (err, user) => {
-            if (err) {
-                // return res.status(401).send("Access token tidak valid");
+            jwt.verify(accessToken, accessTokenKey, (err, user) => {
+
+                req.userId = user.userId;
+                req.userRole = user.userRole;
+
                 next();
-            }
-
-            req.userId = user.userId;
-            req.userRole = user.userRole;
-
-            next();
-        });
-    } catch (error) {
-        // return res.status(401).send(error);
-        next();
+            });
+        } catch (error) {
+                return res.status(401).send();
+        }
     }
-
 }
